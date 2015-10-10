e48d116bcfbc0a1255ef8d78d2c2b8a6af44410c
DollarAkshay <akshay95aradhya@gmail.com>
2015-10-11 04:47:42 +0530

    Ace Coders Code

---
+++ Multiplayer/Ace of Coders/Humans.js
@@ -0,0 +1,110 @@
+﻿// Global Variables
+enemy = []
+friend = []
+gold = []
+
+enemyArcher = []
+enemySoldier = []
+enemyArtillery = []
+enemyTower = []
+enemyMissile = []
+enemyGold = []
+enemyHero = []
+
+friendArcher = []
+friendSoldier = []
+friendArtillery = []
+friendTower = []
+friendMissile = []
+friendGold = []
+friendHero = []
+
+neutralGold = []
+yaks = []
+
+
+createUnits = function(){
+        this.summon("archer")
+}
+    
+  
+commandArmy = function(){
+    for i, f in enumerate(friend)
+        if (f.type == "soldier")
+            commandSoldier(f)
+        else if (f.type == "archer")
+            commandArcher(f)
+        else if (f.type == "artillery")
+            commandArtillery(f)
+        else if (f.type == "arrow-tower")
+            commandTower(f)		
+}
+    
+    
+commandSoldier = function(unit){
+    this.command(unit, "attack", enemyHero[0])
+}
+    
+    
+commandArcher = function(unit){
+    if (len(enemyArcher)>0)
+        this.command(unit, "attack", this.findNearest(enemyArcher))
+    else if (len(enemySoldier)>0)
+        this.command(unit, "attack", this.findNearest(enemySoldier))
+    else
+        this.command(unit, "attack", enemyHero[0])
+}
+        
+        
+commandArtillery = function(unit){
+    if (len(enemyArtillery)>0)
+        this.command(unit, "attack", this.findNearest(enemyArtillery))
+    else if (len(enemyTower)>0)
+        this.command(unit, "attack", this.findNearest(enemyTower))
+}
+        
+        
+commandTower = function(unit){
+    this.command(unit, "attack", this.findNearest(eenemy))
+}
+    
+initialize = function(){
+    this.debug("Time : "+this.now())
+    global enemy
+    enemy = this.findEnemies()
+    global friend = this.findFriends()
+    global gold = this.getControlPoints()
+    
+    for i, e in enumerate(enemy) :
+        //this.debug("Type of enemy "+i+" is : "+e.type)
+        if (e.type == "soldier")
+            enemySoldier.append(e)
+        else if (e.type == "archer")
+            enemyArcher.append(e)
+        else if (e.type == "artillery")
+            enemyArtillery.append(e)
+        else if (e.type == "arrow-tower")
+            enemyTower.append(e)
+        else if (e.type == "goliath")
+            enemyHero.append(e)
+        
+    for i, f in enumerate(friend) :
+        #this.debug("Type of friend "+i+" is : "+f.type)
+        if (f.type == "soldier")
+            friendSoldier.append(f)
+        else if (f.type == "archer")
+            friendArcher.append(f)
+        else if (f.type == "artillery")
+            friendArtillery.append(f)
+        else if (f.type == "arrow-tower")
+            friendTower.append(f)
+        else if (f.type == "goliath")
+            friendHero.append(f)
+			
+}      
+
+while (True){
+    createUnits()
+    initialize()
+    this.say(len(friend))
+    commandArmy()
---
+++ Multiplayer/Ace of Coders/Ogres.js
@@ -0,0 +1,189 @@
+﻿// Global Variables
+var enemy = [];
+var friend = [];
+var gold = [];
+
+var enemyArcher = [];
+var enemySoldier = [];
+var enemyArtillery = [];
+var enemyTower = [];
+var enemyMissile = [];
+var enemyGold = [];
+var enemyHero = [];
+
+var friendArcher = [];
+var friendSoldier = [];
+var friendArtillery = [];
+var friendTower = [];
+var friendMissile = [];
+var friendGold = [];
+var friendHero = [];
+
+var neutralGold = [];
+var yaks = [];
+
+clearAll = function(){
+    enemy = [];
+    friend = [];
+    gold = [];
+    
+    enemyArcher = [];
+    enemySoldier = [];
+    enemyArtillery = [];
+    enemyTower = [];
+    enemyMissile = [];
+    enemyGold = [];
+    enemyHero = [];
+    
+    friendArcher = [];
+    friendSoldier = [];
+    friendArtillery = [];
+    friendTower = [];
+    friendMissile = [];
+    friendGold = [];
+    friendHero = [];
+    
+    neutralGold = [];
+    yaks = [];
+};
+
+var getKeys = function(obj){
+   var keys = [];
+   for(var key in obj){
+      keys.push(key);
+   }
+   return keys;
+};
+
+this.createUnits = function(){
+    while(this.gold > this.costOf("archer")){
+        this.summon("archer");
+    }
+};
+
+this.initialize = function(){
+    this.debug("Time : "+this.now());
+    clearAll();
+    enemy = this.findEnemies();
+    friend = this.findFriends();
+    gold = this.getControlPoints();
+    for (i=0; i<enemy.length; i++) {
+        var e = enemy[i];
+        //this.debug("Type of enemy "+i+" is : "+e.type)
+        if (e.type == "soldier")
+            enemySoldier.push(e);
+        else if (e.type == "archer")
+            enemyArcher.push(e);
+        else if (e.type == "artillery")
+            enemyArtillery.push(e);
+        else if (e.type == "arrow-tower")
+            enemyTower.push(e);
+        else if (e.type == "ice-yaks")
+            yaks.push(e);
+        else if (e.type == "goliath" || e.type=="knight")
+            enemyHero.push(e);
+    }
+        
+    for (i=0; i<friend.length; i++){
+        var f = friend[i];
+        //this.debug("Type of friend "+i+" is : "+f.type)
+        if (f.type == "soldier")
+            friendSoldier.push(f);
+        else if (f.type == "archer")
+            friendArcher.push(f);
+        else if (f.type == "artillery")
+            friendArtillery.push(f);
+        else if (f.type == "arrow-tower")
+            friendTower.push(f);
+        else if (f.type == "goliath")
+            friendHero.push(f);
+    }		
+};
+
+this.commandSoldier = function(unit){
+    if(enemyHero.length > 0)
+        this.command(unit, "attack", enemyHero[0]);
+};
+
+
+this.commandArcher = function(unit){
+    if(friendArcher[0].id==unit.id){
+        this.command(unit, "defend", gold[1].pos);
+    }
+    else if(friendArcher[1].id==unit.id){
+        this.command(unit, "defend", gold[2].pos);
+    }
+    else if (enemyArcher.length>0){
+        this.command(unit, "attack", unit.findNearest(enemyArcher));
+    }
+    else if (enemySoldier.length>0){
+        this.command(unit, "attack", unit.findNearest(enemySoldier));
+    }
+    else if(enemyHero.length > 0){
+        this.command(unit, "attack", enemyHero[0]);
+    }
+};
+
+
+this.commandArtillery = function(unit){
+    if (enemyArtillery.length>0)
+        this.command(unit, "attack", this.findNearest(enemyArtillery));
+    else if (enemyTower.length>0)
+        this.command(unit, "attack", this.findNearest(enemyTower));
+};
+
+
+this.commandTower = function(unit){
+    this.command(unit, "attack", this.findNearest(enemy));
+};
+
+this.commandArmy = function(){
+    
+    for (var i=0; i<friend.length; i++){
+        var f = friend[i];
+        if (f.type == "soldier")
+            this.commandSoldier(f);
+        else if (f.type == "archer")
+            this.commandArcher(f);
+        else if (f.type == "artillery")
+            this.commandArtillery(f);
+        else if (f.type == "arrow-tower")
+            this.commandTower(f);	
+    }
+};
+
+this.showHeroism = function(){
+    if(enemyHero.length > 0){
+        if(this.isReady("stomp") && this.distanceTo(enemyHero[0])<15){
+            this.stomp();
+        }
+        if(this.isReady("throw")){
+            if(enemyArtillery.lenght>0){
+                this.throw(enemyArtillery[0]);
+            }
+            else if(enemyArcher.length>0 && this.distanceTo(this.findNearest(enemyArcher))<=25){
+                this.throw(this.findNearest(enemyArcher));
+            }
+            else if(this.distanceTo(enemyHero[0])<=30){
+                this.throw(enemyHero[0]);
+            }
+        }
+        this.attack(enemyHero[0]);
+    }
+    else{
+        this.say("Take that B!t¢#");
+    }
+};
+
+
+while (true){
+    
+    this.createUnits();
+    this.initialize();
+    this.commandArmy();
+    this.showHeroism();
+}
+
+
+
+
